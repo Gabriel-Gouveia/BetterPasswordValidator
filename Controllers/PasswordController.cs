83441435a63efc469a0911c5d1b864ea902456fe
Gabriel Alves Gouveia <84570338+Gabriel-Gouveia@users.noreply.github.com>
2024-02-28 20:32:33 -0300

    [FIX] Versioning the API

--- Controllers/PasswordController.cs
+++ Controllers/PasswordController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace PasswordValidator.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class PasswordController : ControllerBase
     {
